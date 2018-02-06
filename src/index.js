2dfb513c5b13c83e8ae41918ffa599d509e44fcd
harikanth ghanta <harikanth.ghanta@gmail.com>
2018-02-06 01:53:33 -0800

    added SerachBox

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import CardList from './cardList';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
-import {robots} from './robots.js';
 
-ReactDOM.render(<CardList robots={robots}/>				
-	, document.getElementById('root'));
+
+ReactDOM.render(<App/>,
+	 document.getElementById('root'));
 registerServiceWorker();
 
