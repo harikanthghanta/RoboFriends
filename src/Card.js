d0d1354689352fde60c0901cd0705daee103bec7
harikanth ghanta <harikanth.ghanta@gmail.com>
2018-02-12 21:11:01 -0800

    added scroll bar

--- src/Card.js
+++ src/Card.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Card = ({id,name,email}) => {
 
 	return (
-		<div className='bg-light-yellow dib br3 pa3 ma2 shadow-3 grow '>
+		<div className='bg-light-blue dib br3 pa3 ma2 shadow-3 grow '>
 			<img alt='robot' src={`https://robohash.org/${id}`}/>
 			 <div>
 			 	<h1>{name}</h1>
